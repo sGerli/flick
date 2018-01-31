554ae70926dfbd0e005d2721616a71d2dfda1eba
SPQRInc <roman.lossin@icloud.com>
2018-01-31 13:50:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -58,8 +58,8 @@ return [
     'events' => [
         'view.scripts' => function ($event, $scripts) use ($app) {
 
-			$scripts->register('uikit-grid', 'sgerli/flick:app/assets/uikit/js/components/grid.min.js', 'uikit');
-			$scripts->register('uikit-lightbox', 'sgerli/flick:app/assets/uikit/js/components/lightbox.min.js', 'uikit');
+			$scripts->register('uikit-grid', 'app/assets/uikit/js/components/grid.min.js', 'uikit');
+			$scripts->register('uikit-lightbox', 'app/assets/uikit/js/components/lightbox.min.js', 'uikit');
 		},
     ]
 ];
