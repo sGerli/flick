e6ad3e555fb5480de04593f715cd6a765eb4cbd7
Stefano Gerli <sgerli@igerli.com>
2018-03-05 16:29:44 -0600

    Fix sorting

--- views/index.php
+++ views/index.php
@@ -32,15 +32,15 @@ $photosets = $flickr->getCollection($collectionId)->set ?>
 
     <div class="uk-tab-center uk-margin">
         <ul id="flick-filter" class="uk-tab">
-            <li class="uk-active" data-uk-filter=""><a href=""><?= __('All') ?></a></li>
+            <li class="uk-active" data-uk-filter=""><a href="#"><?= __('All') ?></a></li>
             <?php foreach ($photosets as $photoset) : ?>
-                <li data-uk-filter="<?= $photoset->title ?>"><a href=""><?= __($photoset->title) ?></a></li>
+                <li data-uk-filter="<?= $photoset->title ?>"><a href="#"><?= __($photoset->title) ?></a></li>
             <?php endforeach; ?>
 
         </ul>
     </div>
 
-    <div class="uk-grid uk-grid-width-small-1-1 uk-grid-width-medium-1-3" data-uk-grid="{controls: #flick-filter, gutter: 20}">
+    <div class="uk-grid uk-grid-width-small-1-1 uk-grid-width-medium-1-3" data-uk-grid="{controls: '#flick-filter', gutter: 20}">
         <?php foreach ($photosets as $photoset): ?>
         <?php $photos = $flickr->getPhotosetPhotos($photoset->id); ?>
             <?php foreach ($photos as $photo ): ?>
